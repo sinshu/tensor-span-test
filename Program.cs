659a532521272e3fbfaa717736071955079fb5b1
Nobuaki Tanaka <sinshu.d@gmail.com>
2026-02-17 21:49:37 +0900

    a

--- Program.cs
+++ Program.cs
@@ -1,39 +1,50 @@
-using NumFlat;
-using System.Numerics;
+﻿using NumFlat;
+using System;
 using System.Numerics.Tensors;
-using System.Runtime.CompilerServices;
 
-Mat<double> mat = new(2, 2);
-mat[0, 0] = 1;
-mat[0, 1] = 2;
-mat[1, 0] = 3;
-mat[1, 1] = 4;
-
-TensorSpan<double> tensorSpan = mat.ToTensorSpan();
-
-Console.WriteLine($"matrix: {mat.RowCount}x{mat.ColCount}");
-Console.WriteLine($"tensor shape: [{string.Join(", ", tensorSpan.Lengths.ToArray())}]");
-Console.WriteLine($"tensor[1, 0] = {tensorSpan[1, 0]}");
-
-[InlineArray(2)]
-struct Nint2
+public static class Program
 {
-    private nint _element0;
-}
-
-static class MatTensorSpanExtensions
-{
-    public static TensorSpan<T> ToTensorSpan<T>(this Mat<T> mat)
-        where T : unmanaged, INumberBase<T>
+    public static void Main()
     {
-        Nint2 lengths = default;
-        lengths[0] = mat.RowCount;
-        lengths[1] = mat.ColCount;
+        VecTest();
+        MatTest();
+    }
 
-        Nint2 strides = default;
-        strides[0] = 1;
-        strides[1] = mat.Stride;
+    public static void VecTest()
+    {
+        Vec<double> a = [1, 2, 3];
 
-        return new TensorSpan<T>(mat.Memory.Span, lengths, strides);
+        Vec<double> b = [3, 2, 1];
+
+        var c = new Vec<double>(3);
+
+        // Use a TensorSpan method.
+        Tensor.Multiply(a.AsTensorSpan(), b.AsTensorSpan(), c.AsTensorSpan());
+
+        Console.WriteLine(c);
+    }
+
+    public static void MatTest()
+    {
+        Mat<double> a =
+        [
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9],
+        ];
+
+        Mat<double> b =
+        [
+            [9, 8, 7],
+            [6, 5, 4],
+            [3, 2, 1],
+        ];
+
+        var c = new Mat<double>(3, 3);
+
+        // Use a TensorSpan method.
+        Tensor.Multiply(a.AsTensorSpan(), b.AsTensorSpan(), c.AsTensorSpan());
+
+        Console.WriteLine(c);
     }
 }
